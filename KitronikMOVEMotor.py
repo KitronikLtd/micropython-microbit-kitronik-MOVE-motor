3c1814df18d4fbb701cb7216164082d0e480136b
Jack Faulkner <jack.faulkner@kitronik.co.uk>
2022-11-08 11:58:42 +0000

    Condensed motor control, added led control

--- KitronikMOVEMotor.py
+++ KitronikMOVEMotor.py
@@ -21,7 +21,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-from microbit import i2c, pin3, pin12, pin15, pin16, display, sleep
+from microbit import i2c, pin3, pin8, pin12, pin15, pin16, display, sleep
 from neopixel import NeoPixel
 
 # A module to simplify the driving o the motors on Kitronik :MOVE Motor buggy with micro:bit
@@ -44,6 +44,8 @@ SERVO_MAX_PULSE = 2500 #microsec
 SERVO_DEGREE_RANGE = 180
 SERVO_DEG_TO_uS = (SERVO_MAX_PULSE - SERVO_MIN_PULSE) / SERVO_DEGREE_RANGE
 SERVO_PWM_PERIOD = 20 #ms
+
+LEDS_NUMBER = 4
 
 class MOVEMotor:
 
@@ -94,114 +96,101 @@ class MOVEMotor:
         pin15.set_analog_period(SERVO_PWM_PERIOD)
         pin16.set_analog_period(SERVO_PWM_PERIOD)
 
-    # A couple of 'raw' speed functions for the motors.
-    # these functions expect speed -255 -> +255
-    def LeftMotor(self, speed):
+        # Setup LEDs
+        self.leds = NeoPixel(pin8, LEDS_NUMBER)
+
+    # Function to set the requested motor running in chosen direction at a set speed.
+    # motor = l for left and motor = r for right
+    # direction = f for forward and direction = r for reverse
+    # speed between 0 and 255
+    def motorOn(self, motor, direction, speed):
+        speed = int(speed)
+        if speed > 255:
+            speed = 255
+        elif speed < 0:
+            speed = 0
+        
         if self.moveMotorVersion != 31:
-            speed = int(speed)
-            if speed > 255:
-                speed = 255
-            elif speed < -255:
-                speed = -255
+            # V1 to V2 :MOVE Motor
             motorForward = bytearray([0, 0])
             motorBackward = bytearray([0, 0])
-            motorForward[0] = LEFT_MOTOR
-            motorBackward[0] = LEFT_MOTOR + 1
-            if speed > 0:
-                # going forwards
+            if motor == "l":
+                # Left motor
+                motorForward[0] = LEFT_MOTOR
+                motorBackward[0] = LEFT_MOTOR + 1
+            elif motor == "r":
+                # Right motor
+                motorForward[0] = RIGHT_MOTOR + 1
+                motorBackward[0] = RIGHT_MOTOR
+            if direction == "f":
+                # Going forwards
                 motorForward[1] = speed
-            elif speed < 0: 
-                # going backwards
-                motorBackward[1] = speed * -1
+            elif direction == "r":
+                # Going backwards
+                motorBackward[1] = speed
             i2c.write(CHIP_ADDR, motorForward, False)
             i2c.write(CHIP_ADDR, motorBackward, False)
-
+        
         else:
-            speed = int(speed)
-            if speed > 255:
-                speed = 255
-            elif speed < -255:
-                speed = -255
-            motor = bytearray([0, 0, 0])
+            # V3 :MOVE Motor
+            motorBuf = bytearray([0, 0, 0])
             motorJump = bytearray([0, 0, 0])
-            if speed > 0:
-                # going forwards
-                motor[0] = speed
-                motorJump[0] = 255
-            elif speed < 0: 
-                # going backwards
-                motor[1] = speed * -1
-                motorJump[1] = 255
-            else:
-                # stopping
-                motor[2] = 255
-            self.ws2811[1] = (motorJump[0], motorJump[1], motorJump[2])
+            wsIndex = 0
+            if motor == "l":
+                # Left motor
+                wsIndex = 1
+                if direction == "f":
+                    # Going forwards
+                    motorBuf[0] = speed
+                    motorJump[0] = 255
+                elif direction == "r":
+                    # Going backwards
+                    motorBuf[1] = speed
+                    motorJump[1] = 255
+            elif motor == "r":
+                # Right motor
+                wsIndex = 0
+                if direction == "f":
+                    # Going forwards
+                    motorBuf[1] = speed
+                    motorJump[1] = 255
+                elif direction == "r":
+                    # Going backwards
+                    motorBuf[0] = speed
+                    motorJump[0] = 255
+            self.ws2811[wsIndex] = (motorJump[0], motorJump[1], motorJump[2])
             self.ws2811.show()
             sleep(1) # 1 ms
-            self.ws2811[1] = (motor[0], motor[1], motor[2])
+            self.ws2811[wsIndex] = (motorBuf[0], motorBuf[1], motorBuf[2])
             self.ws2811.show()
 
-    #speed -255 -> +255
-    def RightMotor(self,speed):
+    # A function that stop a given motor
+    # motor = l for left and motor = r for right
+    def motorOff(self, motor):
         if self.moveMotorVersion != 31:
-            speed = int(speed)
-            if speed > 255:
-                speed = 255
-            elif speed < -255:
-                speed = -255
-            motorForward = bytearray([0, 0])
-            motorBackward = bytearray([0, 0])
-            motorForward[0] = RIGHT_MOTOR + 1
-            motorBackward[0] = RIGHT_MOTOR
-            if speed > 0:
-                # going forwards
-                motorForward[1] = speed
-            elif speed < 0: 
-                # going backwards
-                motorBackward[1] = speed * -1
-            i2c.write(CHIP_ADDR, motorForward, False)
-            i2c.write(CHIP_ADDR, motorBackward, False)
-
-        else:
-            speed = int(speed)
-            if speed > 255:
-                speed = 255
-            elif speed < -255:
-                speed = -255
-            motor = bytearray([0, 0, 0])
-            motorJump = bytearray([0, 0, 0])
-            if speed > 0:
-                # going forwards
-                motor[1] = speed
-                motorJump[1] = 255
-            elif speed < 0: 
-                # going backwards
-                motor[0] = speed * -1
-                motorJump[0] = 255
-            else:
-                # stopping
-                motor[2] = 255
-            self.ws2811[0] = (motorJump[0], motorJump[1], motorJump[2])
-            self.ws2811.show()
-            sleep(1) # 1 ms
-            self.ws2811[0] = (motor[0], motor[1], motor[2])
-            self.ws2811.show()
-
-    # A function that stops both motors, rather than having to call Left and Right with zero speed.
-    def StopMotors(self):
-        if self.moveMotorVersion != 31:
+            # V1 to V2 :MOVE Motor
             stopBuffer = bytearray([0, 0])
-            stopBuffer[0] = LEFT_MOTOR
-            i2c.write(CHIP_ADDR, stopBuffer, False)
-            stopBuffer[0] = LEFT_MOTOR + 1
-            i2c.write(CHIP_ADDR, stopBuffer, False)
-            stopBuffer[0] = RIGHT_MOTOR
-            i2c.write(CHIP_ADDR, stopBuffer, False)
-            stopBuffer[0] = RIGHT_MOTOR + 1
-            i2c.write(CHIP_ADDR, stopBuffer, False)
+            if motor == "l":
+                # Left motor
+                stopBuffer[0] = LEFT_MOTOR
+                i2c.write(CHIP_ADDR, stopBuffer, False)
+                stopBuffer[0] = LEFT_MOTOR + 1
+                i2c.write(CHIP_ADDR, stopBuffer, False)
+            elif motor == "r":
+                # Right motor
+                stopBuffer[0] = RIGHT_MOTOR
+                i2c.write(CHIP_ADDR, stopBuffer, False)
+                stopBuffer[0] = RIGHT_MOTOR + 1
+                i2c.write(CHIP_ADDR, stopBuffer, False)
+        
         else:
-            self.ws2811[0] = (0, 0, 255)
-            self.ws2811[1] = (0, 0, 255)
+            # V3 :MOVE Motor
+            if motor == "l":
+                # Left motor
+                self.ws2811[1] = (0, 0, 255)
+            elif motor == "r":
+                # Right motor
+                self.ws2811[0] = (0, 0, 255)
             self.ws2811.show()
     
     # Servo Control
@@ -226,3 +215,18 @@ class MOVEMotor:
             pin15.write_analog(duty)
         elif  servo == 2:
             pin16.write_analog(duty)
+    
+    # LED Control
+    def setLED(self, led, colour):
+        if led < 0:
+            led = 0
+        elif led > LEDS_NUMBER - 1:
+            led = LEDS_NUMBER - 1
+        self.leds[led] = colour
+    
+    def setLEDs(self, colour):
+        for i in range(LEDS_NUMBER):
+            self.setLED(i, colour)
+    
+    def showLEDs(self):
+        self.leds.show()
